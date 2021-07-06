bb1a90246746fa02be0013a9122bc7ce3f6e7cb1
Arbin20 <arbin.qazimi@outlook.com>
2021-07-06 22:28:50 +0200

    updated

--- school-admin/includes/header.php
+++ school-admin/includes/header.php
@@ -88,13 +88,13 @@
 						</li>
 					</ul>
 					<ul class="nav navbar-nav navbar-right navbar-user">
-						<li><a href="../index.php" target="_blank">Visit Website</a></li>
+						<li><a href="../index.php" target="_blank">Kthehu në Uebfaqe</a></li>
 						<li class="dropdown user-dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo get_name_by_session(); ?> <b class="caret"></b></a>
 							<ul class="dropdown-menu">
-								<li><a href="admin_profile.php"><i class="fa fa-user"></i>  Profile</a></li>
+								<li><a href="admin_profile.php"><i class="fa fa-user"></i>  Profili</a></li>
 								<li class="divider"></li>
-								<li><a href="../logout.php"><i class="fa fa-power-off"></i> Log Out</a></li>
+								<li><a href="../logout.php"><i class="fa fa-power-off"></i> Çkyçu nga sistemi</a></li>
 							</ul>
 						</li>
 					</ul>
